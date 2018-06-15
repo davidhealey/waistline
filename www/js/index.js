77819af98d2de0013800da452001e539739922ad
d-healey <dave41264@hotmail.com>
2018-06-16 00:14:45 +0100

    Slight refactoring for theme selection

--- www/js/index.js
+++ www/js/index.js
@@ -48,6 +48,7 @@ var app = {
       defaultCallback(data);
       app.strings = $.localize.data["locales/locale"];
     }
+
     $("[data-localize]").localize("locales/locale", opts)
     console.log($.localize);
 
@@ -64,9 +65,10 @@ var app = {
 
     if (this.storage.getItem("theme") == undefined)
     {
-	this.storage.setItem("theme", "default");
+	     this.storage.setItem("theme", "default");
     }
-    initTheme();
+
+    setTheme(app.storage.getItem("theme")); //Restore theme
     changeDate(app.date); //Default to current date
   },
 };
@@ -731,49 +733,39 @@ $("#settingsPage").on("pagebeforeshow", function(event, ui)
 //Initialize stylesheet on startup
 function initTheme(){
     theme = app.storage.getItem("theme");
-    switch(true) {
-    case( (theme == "night") ):
-    	$('link[href*="css/themes/black.css"]').prop('disabled', false);
-    	$('link[href*="css/themes/amoled.css"]').prop('disabled', true);
-    	break;
-    case( (theme == "amoled") ):
-    	$('link[href*="css/themes/black.css"]').prop('disabled', true);
-    	$('link[href*="css/themes/amoled.css"]').prop('disabled', false);
-    	break;
-    case( (theme == "default") ):
-    	$('link[href*="css/themes/black.css"]').prop('disabled', true);
-    	$('link[href*="css/themes/amoled.css"]').prop('disabled', true);
-    	break;
-    }
+
 }
 
 // Change Theme by Selector
 $('#settingsPage #theme').change( function(){
-      selection = $("#settingsPage #theme").val();
-    switch(true) {
-    case( (selection == "night")):
-    	$('link[href*="css/themes/black.css"]').prop('disabled', false);
-    	$('link[href*="css/themes/amoled.css"]').prop('disabled', true);
-    	break;
-    case( (selection == "amoled")):
-    	$('link[href*="css/themes/black.css"]').prop('disabled', true);
-    	$('link[href*="css/themes/amoled.css"]').prop('disabled', false);
-    	break;
-    case(  (selection == "default")):
-    	$('link[href*="css/themes/black.css"]').prop('disabled', true);
-    	$('link[href*="css/themes/amoled.css"]').prop('disabled', true);
-    	break;
-    }
-    app.storage.setItem("theme", selection);
-
+    setTheme($("#settingsPage #theme").val());
 });
+
+function setTheme(theme)
+{
+  switch(true) {
+  case( (theme == "night")):
+    $('link[href*="css/themes/black.css"]').prop('disabled', false);
+    $('link[href*="css/themes/amoled.css"]').prop('disabled', true);
+    break;
+  case( (theme == "amoled")):
+    $('link[href*="css/themes/black.css"]').prop('disabled', true);
+    $('link[href*="css/themes/amoled.css"]').prop('disabled', false);
+    break;
+  case(  (theme == "default")):
+    $('link[href*="css/themes/black.css"]').prop('disabled', true);
+    $('link[href*="css/themes/amoled.css"]').prop('disabled', true);
+    break;
+  }
+  app.storage.setItem("theme", theme);
+}
 
 //Save user's settings
 function saveUserSettings()
 {
-  app.storage.setItem("weight", $('#settingsPage #weight').val());
-  app.storage.setItem("calorieGoal", Math.round($('#settingsPage #calorieGoal').val()));
-  app.storage.setItem("goalIsMin", $("#settingsPage #goalIsMin").prop("checked"));
+    app.storage.setItem("weight", $('#settingsPage #weight').val());
+    app.storage.setItem("calorieGoal", Math.round($('#settingsPage #calorieGoal').val()));
+    app.storage.setItem("goalIsMin", $("#settingsPage #goalIsMin").prop("checked"));
     app.storage.setItem("scanImages", $("#settingsPage #scanImages").prop("checked"));
     app.storage.setItem("theme", $("#settingsPage #theme").prop("selected"));
 
@@ -782,4 +774,3 @@ function saveUserSettings()
 
   $(":mobile-pagecontainer").pagecontainer("change", "#home");
 }
-
