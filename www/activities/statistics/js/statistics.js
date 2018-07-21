0e29c3deb486f8740925bc68b98be6d0b52c779a
d-healey <dave41264@hotmail.com>
2018-07-21 23:49:04 +0100

    Added colours to diary statistics

--- www/activities/statistics/js/statistics.js
+++ www/activities/statistics/js/statistics.js
@@ -43,6 +43,7 @@ var statistics = {
     //Get diary stats for today
     diary.getStats(dateTime, function(data) {
 
+      var colour = "";
       var html = "<ons-carousel swipeable auto-scroll auto-refresh>";
 
       //Sort goals alphabetically
@@ -53,8 +54,19 @@ var statistics = {
 
       for (g in goals)
       {
+        if (g == "weight") continue; //Weight is handled separately
+
+        //Set colour for remaining text
+        data.nutrition[g] < data.goals[g] ? colour = "green" : colour = "red";
+
+        //For calories the colour is flipped if the user checked the gain weight option
+        if (g == "calories" && goals.weight.gain == true)
+        {
+          data.nutrition[g] > data.goals[g] ? colour = "green" : colour = "red";
+        }
+
         html += "<ons-carousel-item>";
-        html += "<h2 style='text-align:center;'>"+g+"</h2>";
+        html += "<h2 style='text-align:center;'>"+g.charAt(0).toUpperCase() + g.slice(1)+"</h2>";
         html += "<ons-row>";
         html += "<ons-col width='33%' style='text-align:center;'>Goal</ons-col>"
         html += "<ons-col width='33%' style='text-align:center;'>Used</ons-col>"
@@ -63,7 +75,7 @@ var statistics = {
         html += "<ons-row>";
         html += "<ons-col width='33%' style='text-align:center;'>"+data.goals[g]+"</ons-col>";
         html += "<ons-col width='33%' style='text-align:center;'>"+data.nutrition[g]+"</ons-col>";
-        html += "<ons-col width='33%' style='text-align:center;'>"+data.remaining[g]+"</ons-col>";
+        html += "<ons-col width='33%' style='text-align:center; color:"+colour+";'>"+data.remaining[g]+"</ons-col>";
         html += "</ons-row>";
         html += "</ons-carousel-item>";
       }
