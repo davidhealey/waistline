9e4a6be48e983443bfdb47e55cec1a35328ecd90
d-healey <dave41264@hotmail.com>
2018-07-25 15:18:09 +0100

    Added settings to customise meal names

--- www/activities/settings/js/settings.js
+++ www/activities/settings/js/settings.js
@@ -1,21 +1,59 @@
 var settings = {
 
-  save : function(control)
+  saveSwitches : function(control)
   {
     app.storage.setItem(control.id, control.checked);
   },
 
-  load : function()
+  loadSwitches : function()
   {
     var switches = $("#settings-page ons-switch");
 
-    for (i = 0; i < switches.length; i++)
+    for (var i = 0; i < switches.length; i++)
     {
       switches[i].checked = app.storage.getItem(switches[i].id);
     }
+  },
+
+  saveDiarySettings : function()
+  {
+    var inputs = $("#diary-settings ons-input");
+    var mealNames = [];
+
+    for (var i = 0; i < inputs.length; i++)
+    {
+      mealNames.push(inputs[i].value);
+    }
+
+    app.storage.setItem("meal-names", JSON.stringify(mealNames));
+  },
+
+  loadDiarySettings : function()
+  {
+    var mealNames = JSON.parse(app.storage.getItem("meal-names"));
+
+    if (mealNames)
+    {
+      var inputs = $("#diary-settings ons-input");
+
+      for (var i = 0; i < mealNames.length; i++)
+      {
+        inputs[i].value = mealNames[i];
+      }
+    }
   }
+
 }
 
 $(document).on("show", "#settings-page", function(e){
-  settings.load();
+  settings.loadSwitches();
+});
+
+$(document).on("show", "#diary-settings", function(e){
+  settings.loadDiarySettings();
+});
+
+$(document).on("tap", "#diary-settings #submit", function(e){
+  settings.saveDiarySettings();
+  //nav.popPage();
 });
