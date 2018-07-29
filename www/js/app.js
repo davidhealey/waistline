0ce5e4af13fd5179cfc7fa273a58109138d02084
d-healey <dave41264@hotmail.com>
2018-07-29 01:44:32 +0100

    Added function to always get a time at midnight

--- www/js/app.js
+++ www/js/app.js
@@ -110,13 +110,20 @@ var app = {
       }
     });
   },
+
+  getDateAtMidnight : function(date)
+  {
+    newDate = new Date(date);
+    newDate.setHours(0-(newDate.getTimezoneOffset()/60), 0, 0, 0);
+    return newDate;
+  },
 };
 
 ons.ready(function() {
   console.log("Cordova Ready");
   app.initialize();
   if (app.storage.getItem("disable-animation")) ons.disableAnimations(); //Disable all animations if setting enabled
-  nav.resetToPage("activities/statistics/views/statistics.html");
+  nav.resetToPage("activities/diary/views/diary.html");
 });
 
 //Localize when any page is initialized
