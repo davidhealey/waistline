0cd4ac9d1f79abe99b24f2ee057884441ce894d9
d-healey <dave41264@hotmail.com>
2018-07-29 01:11:19 +0100

    Bit of styling

--- platforms/browser/www/js/app.js
+++ platforms/browser/www/js/app.js
@@ -116,7 +116,7 @@ ons.ready(function() {
   console.log("Cordova Ready");
   app.initialize();
   if (app.storage.getItem("disable-animation")) ons.disableAnimations(); //Disable all animations if setting enabled
-  nav.resetToPage("activities/statistics/views/statistics.html");
+  nav.resetToPage("activities/diary/views/diary.html");
 });
 
 //Localize when any page is initialized
