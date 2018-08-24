ceaf95b384c4e4de87ab3afb52d7c35a909076c4
d-healey <dave41264@hotmail.com>
2018-08-25 00:56:13 +0100

    Fixed food list submit button context issue

--- www/activities/food-list/js/food-list.js
+++ www/activities/food-list/js/food-list.js
@@ -559,6 +559,9 @@ $(document).on("init", "#food-list-page", function(e){
 
 $(document).on("keyup", "#food-list-page #filter", function(e){
 
+  $("#food-list-page ons-toolbar-button#submit").hide(); //Hide submit button until items are checked
+  $("#food-list-page ons-toolbar-button#scan").show(); //show scan button
+
   if (this.value == "") //Search box cleared, reset the list
   {
     foodList.fillListFromDB()
