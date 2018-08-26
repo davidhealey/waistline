2ce1bd4a8cf3e5a5bce4daef60714c296328fc30
d-healey <dave41264@hotmail.com>
2018-08-26 18:37:56 +0100

    Changed version number

--- platforms/android/app/build/generated/source/buildConfig/release/com/waist/line/BuildConfig.java
+++ platforms/android/app/build/generated/source/buildConfig/release/com/waist/line/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.waist.line";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 20200;
-  public static final String VERSION_NAME = "2.2.0";
+  public static final int VERSION_CODE = 20201;
+  public static final String VERSION_NAME = "2.2.1";
 }
