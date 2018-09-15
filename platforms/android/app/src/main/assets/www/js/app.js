9fc41e64ed74c0704d637c2ab68bb288a8f7d344
davidhealey <dave41264@hotmail.com>
2018-09-15 17:15:44 +0100

    Separated non GPL helper functions into separate file

--- platforms/android/app/src/main/assets/www/js/app.js
+++ platforms/android/app/src/main/assets/www/js/app.js
@@ -186,10 +186,3 @@ ons.ready(function() {
 $(document).on("init", "ons-page", function(e){
   $("[data-localize]").localize("locales/locale");
 });
-
-// Array Remove - By John Resig (MIT Licensed)
-Array.prototype.remove = function(from, to) {
-  var rest = this.slice((to || from) + 1 || this.length);
-  this.length = from < 0 ? this.length + from : from;
-  return this.push.apply(this, rest);
-};
---
+++ www/js/helpers.js
@@ -0,0 +1,10 @@
+/*
+The helper functions in this file are available under various GPL compatible free licenses (each function has a comment above to state its license).
+*/
+
+// Array Remove - By John Resig (MIT Licensed)
+Array.prototype.remove = function(from, to) {
+  var rest = this.slice((to || from) + 1 || this.length);
+  this.length = from < 0 ? this.length + from : from;
+  return this.push.apply(this, rest);
+};
