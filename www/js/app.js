ee1f2211587083ab206c181c61e34a763b13c43c
davidhealey <dave41264@hotmail.com>
2018-10-07 22:03:52 +0100

    Fixed missing character

--- www/js/app.js
+++ www/js/app.js
@@ -89,10 +89,9 @@ var app = {
   },
 
   //UUID generator
-  uuidv4 : function() {
-    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-    )
+  uuidv4 : function()
+  {
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c => (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16));
   },
 
   setTheme : function(theme)
